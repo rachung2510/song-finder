2b01ee0aead7f37867b4c3bb21e54213f5c37ffd
rachung2510 <chungrc.rachel@gmail.com>
2026-07-03 19:34:53 +0000

    remove files unrelated to song-finder

--- scripts/prune.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# delete logs older than 30 days
-find /var/www/html/nextcloud_sacm/scripts/logs -type f -mtime +30 -delete
-
-# delete tmp files after 1 week
-find /mnt/NextcloudSacmData/admin/files/tmp -type f -ctime +3 -delete
-sudo -u www-data php /var/www/html/nextcloud_sacm/occ files:scan --path admin/files/tmp
